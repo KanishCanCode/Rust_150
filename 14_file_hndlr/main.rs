0e8991b5abbf065545e1972a76e70c1e1625fbda
Kanish <147413494+KanishCanCode@users.noreply.github.com>
2025-09-28 18:18:43 +0530

    14

---
+++ 14_file_hndlr/main.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::{self , BufRead,BufReader};
+
+fn main()->io::Result<()>{
+    let file = File::open("SRS_KP.txt")?;
+    let reader = BufReader::new(file);
+    for line in reader.lines(){
+        let line=line?;
+        println!("{}",line);
+    }
+Ok(())
+}
